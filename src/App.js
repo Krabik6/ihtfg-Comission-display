b1ac689b6081a5505c1a73c39359c1db64fab60d
Krabik6 <87415587+Krabik6@users.noreply.github.com>
2022-04-23 00:50:00 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+// eslint-disable-next-line no-unused-vars
+// import {hre} from "hardhat";
 
 function App() {
+  const [fee, setFee] = useState();
+  const [polygonGasPrice, setgasPrice] = useState();
+  async function GetFee() {
+    const polygonProvider = await ethers.getDefaultProvider(
+      'https://rpc.ankr.com/polygon	'
+    );
+    // const lastPolygonBlock = await polygonProvider.getBlockNumber();
+    // console.log('Polygon last block: ' + lastPolygonBlock);
+    const polygonGasPrice = await polygonProvider.getGasPrice();
+    const getgasPrice = ethers.utils.formatUnits(polygonGasPrice, 'gwei'); //36
+    function gasPriceData() {
+      return setgasPrice(getgasPrice);
+    }
+    gasPriceData();
+
+    const feexData = await polygonProvider.getFeeData();
+    const getFeeData = ethers.utils.formatUnits(feexData.maxFeePerGas, 'gwei');
+    function feeData() {
+      return setFee(getFeeData);
+    }
+    feeData();
+  }
+
+  const [feeEth, setFeeEth] = useState();
+  const [ethereumGasOrice, ethereumSetGasPrice] = useState();
+  async function getEthFee() {
+    const polygonProvider = await ethers.getDefaultProvider(
+      'https://rpc.ankr.com/eth'
+    );
+    // const lastPolygonBlock = await polygonProvider.getBlockNumber();
+    // console.log('Polygon last block: ' + lastPolygonBlock);
+    const polygonGasPrice = await polygonProvider.getGasPrice();
+    const getgasPrice = ethers.utils.formatUnits(polygonGasPrice, 'gwei'); //36
+    function gasPriceData() {
+      return ethereumSetGasPrice(getgasPrice);
+    }
+    gasPriceData();
+
+    const feexData = await polygonProvider.getFeeData();
+    const getFeeData = ethers.utils.formatUnits(feexData.maxFeePerGas, 'gwei');
+    function feeData() {
+      return setFeeEth(getFeeData);
+    }
+    feeData();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+
+      <div className='around'>
+      <div className='gorizont'>
+        <button onClick={GetFee}>Get Fee</button>
+        
+        <p>fee: {fee} </p>
+        <p>GasPrice: {polygonGasPrice} </p>
+        </div>
+
+        <div className='gorizont'>
+        <button onClick={getEthFee}>Get Ethereum Fee</button>
+        
+        <p>fee: {feeEth} </p>
+        <p>GasPrice: {ethereumGasOrice} </p>
+        </div>
+        </div>
       </header>
     </div>
   );
