ce2032998adad1e3c8157372e7c7c8d93dedbfaa
Krabik6 <lev1237oo@gmail.com>
2022-04-24 22:32:38 +0400

    master

--- src/App.js
+++ src/App.js
@@ -1,22 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
 function App() {
+    const [fee, setFee] = useState();
+  const [polygonGasPrice, setgasPrice] = useState();
+  async function polygonComission() {
+    const provider = await ethers.getDefaultProvider(
+      'https://rpc.ankr.com/polygon'
+    );
+    const gasPrice = await provider.getGasPrice(); 
+    const fGasPrice = ethers.utils.formatUnits(gasPrice, 'gwei'); //36
+    function gasPriceData() {
+      return setgasPrice(fGasPrice);
+    }
+    gasPriceData();
+
+    const feeData = await provider.getFeeData();
+    const getFeeData = ethers.utils.formatUnits(feeData.maxFeePerGas, 'gwei');
+    function fFeeData() {
+      return setFee(getFeeData);
+    }
+    fFeeData();
+  }
+
+  useEffect(() => {
+    const polygonInterval = setInterval(polygonComission, 5000)
+    
+    return () => {
+    clearInterval(polygonInterval)
+    }
+    }, [fee, polygonGasPrice])
+
+  const [feeEth, setFeeEth] = useState();
+  const [ethereumGasPrice, ethereumSetGasPrice] = useState();
+  async function ethereumComission() {
+    const provider = await ethers.getDefaultProvider(
+      'https://rpc.ankr.com/eth'
+    );
+    // const lastPolygonBlock = await polygonProvider.getBlockNumber();
+    // console.log('Polygon last block: ' + lastPolygonBlock);
+    const gasPrice = await provider.getGasPrice();
+    const fGasPrice = ethers.utils.formatUnits(gasPrice, 'gwei'); //36
+    function gasPriceData() {
+      return ethereumSetGasPrice(fGasPrice);
+    }
+    gasPriceData();
+
+    const feeData = await provider.getFeeData();
+    const getFeeData = ethers.utils.formatUnits(feeData.maxFeePerGas, 'gwei');
+       setFeeEth(getFeeData);
+      //  setInterval(polygonComission, 5000)
+      //  setInterval(ethereumComission, 5000)
+  }
+
+  useEffect(() => {
+    const polygonInterval = setInterval(ethereumComission, 5000)
+    
+    return () => {
+    clearInterval(polygonInterval)
+    }
+    }, [feeEth, polygonGasPrice])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+
+
+
+      <div className='around'>
+      <div className='gorizont'>
+        <p>fee: {fee} </p>
+        <p>GasPrice: {polygonGasPrice} </p>
+        </div>
+
+        <div className='gorizont'>
+        <p>fee: {feeEth} </p>
+        <p>GasPrice: {ethereumGasPrice} </p>
+        </div>
+        </div>
+        
       </header>
     </div>
   );
